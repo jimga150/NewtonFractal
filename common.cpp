4a784c02b5cdbba0551388c4b12d4ee309687838
jimga150 <jimbosned@gmail.com>
2022-05-23 00:41:40 -0400

    flip imaginary axis on screen to make visual sense

--- common.cpp
+++ common.cpp
@@ -2,10 +2,10 @@
 
 complex qpointfToComplex(QPointF p){
     //x is the real axis, y is the imaginary axis
-    return complex(p.x(), p.y());
+    return complex(p.x(), -p.y());
 }
 
 QPointF complexToQPointF(complex c){
     //x is the real axis, y is the imaginary axis
-    return QPointF(c.real(), c.imag());
+    return QPointF(c.real(), -c.imag());
 }
